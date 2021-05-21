e8663230fc364382aacd8347d8b85df17feecdb1
Ian <isford7@gmail.com>
2021-05-21 15:25:11 -0500

    Server started, along with JQ and CSS

---
+++ server/public/scripts/client.js
@@ -0,0 +1,6 @@
+console.log('JS Loaded');//Test JS Load
+
+$(handleReady);//Run JQ on load
+
+function handleReady(){//JQ loaded Test
+    console.log('JQ Loaded')}
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');//requires express install
+const bodyParser = require('body-parser');//allows us to use postman
+//const { watch } = require('fs'); not sure where this came from
+
+//make a server called app
+const app = express();
+const PORT = 5000;
+
+//So when I go to to localhost:5000 it will be static
+app.use(express.static('server/public'));
+//body parser for postman
+app.use(bodyParser.urlencoded({extended : true}))
+
+//listener for connections
+app.listen(PORT, () => {
+    console.log('RUNNING ON PORT:', PORT)
+});
